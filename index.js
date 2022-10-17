c45690e68ba03b9aed253332cfdd91af0e35fbcb
klchanav <31667651+klchanav@users.noreply.github.com>
2022-10-17 18:21:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-// Import packages
 const express = require("express");
-const home = require("./routes/home");
-
-// Middlewares
 const app = express();
-app.use(express.json());
+const http = require("http");
+const home = require("./routes/home");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// Routes
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
 app.use("/home", home);
 
-// connection
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
